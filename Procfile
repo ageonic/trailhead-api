e660919ac9f91efe8ab9bc7f79a1651b3154d7ea
Aaron George <75114114+ang3orge@users.noreply.github.com>
2020-12-29 02:05:55 -0500

    API setup (#1)
    * Add python-dotenv as a dev dependency
    
    * Setup basic api blueprint and routes
    
    * Prepare project for deployment to Heroku
    
    * Add GitHub action to install poetry and run pytest

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app:trailhead_api
--- trailhead_api/__init__.py
+++ trailhead_api/__init__.py
@@ -1,1 +1,16 @@
-__version__ = '0.1.0'
+import os
+from flask import Flask
+
+__version__ = "0.1.0"
+
+
+def create_app():
+    # initialize the flask app
+    app = Flask(__name__)
+
+    # register flask blueprints
+    from trailhead_api.api import bp as api_bp
+
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    return app
