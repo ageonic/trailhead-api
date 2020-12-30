bbb4d9887b5426e8167502d543ba57eed7713ae9
Aaron George <angeorge.dev@gmail.com>
2020-12-29 23:18:10 -0500

    Setup flask-cors to allow cross origin access to api

--- trailhead_api/__init__.py
+++ trailhead_api/__init__.py
@@ -1,12 +1,19 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 __version__ = "0.1.0"
+
+# initialize global extensions
+cors = CORS()
 
 
 def create_app():
     # initialize the flask app
     app = Flask(__name__)
+
+    # initialize flask extensions with app
+    cors.init_app(app, resources=r"/api/*")
 
     # register flask blueprints
     from trailhead_api.api import bp as api_bp
